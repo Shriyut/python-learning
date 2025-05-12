47047f79e0ce929f7fa98acf086b793385ced8f7
shriyut <shriyutjha21@gmail.com>
2025-05-12 16:57:15 +0530

    Sorting

--- sorting/lexiographical_sort.py
+++ sorting/lexiographical_sort.py
@@ -3,7 +3,7 @@ def lexicographical_sort(values):
     return sorted(values)
 
 # Example usage
-values = ["apple", "apple#2025-05-12#1", "apple#2025-05-13", "date", "01", "1", "2", "1.1"]
+values = ["apple", "apple#2025-05-12#1", "apple#2025-05-13", "date", "01", "1", "2", "1.1", "3", "20", "03"]
 sorted_values = lexicographical_sort(values)
 
 print("Original list:", values)
